12b72f6d8bf628e7901a3ca7b11f238adb28a40e
Timur <144324698+MaksiTimur@users.noreply.github.com>
2024-03-08 14:03:20 +0400

    Added Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,15 @@
+import './Header.css';
+import { NavLink } from 'react-router-dom';
+import { FaGithub } from "react-icons/fa6";
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <NavLink className="logo" to={'/'}>Kanbanika</NavLink>
+            <a className='icon-link' href='https://github.com/MaksiTimur/Kanbanika' target='_blank'>
+                <FaGithub className='icon' />
+            </a>
+        </header>
     )
 }
 
