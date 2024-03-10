5c25800b90e090b2954d860f937e3672178552b8
Timur <144324698+MaksiTimur@users.noreply.github.com>
2024-03-10 22:05:50 +0400

    Creating, saving and loading boards (Slice)

--- src/redux/slices/boardsSlice.js
+++ src/redux/slices/boardsSlice.js
@@ -1,17 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 export const boardsSlice = createSlice({
 	name: 'boards',
-	initialState: {
-
-	},
+	initialState: JSON.parse(localStorage.getItem('boards')) ?? [],
 	reducers: {
+		create: (state, action) => {
+			const newBoard = { title: action.payload.title, id: self.crypto.randomUUID() };
 
+			state.push(newBoard);
+
+			localStorage.setItem('boards', JSON.stringify(current(state)));
+		}
 	}
 });
 
 export const {
-
+	create
 } = boardsSlice.actions;
 
 export default boardsSlice.reducer;
