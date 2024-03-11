32092a35bbb5fdc2f18405ec453b6e563a928c63
Timur <144324698+MaksiTimur@users.noreply.github.com>
2024-03-11 21:07:14 +0400

    Creating, saving and loading tasks per column

--- src/redux/slices/tasksSlice.js
+++ src/redux/slices/tasksSlice.js
@@ -1,17 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 export const tasksSlice = createSlice({
 	name: 'tasks',
-	initialState: {
-
-	},
+	initialState: JSON.parse(localStorage.getItem('tasks')) ?? [],
 	reducers: {
+		create: (state, action) => {
+			const newTask = { title: action.payload.title, id: self.crypto.randomUUID(), column: action.payload.column };
 
+			state.push(newTask);
+
+			localStorage.setItem('tasks', JSON.stringify(current(state)));
+		}
 	}
 });
 
 export const {
-
+	create
 } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
