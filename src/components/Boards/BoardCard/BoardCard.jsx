74955180a7c7d77add10f7e109ea8f6af2061ecd
Timur <144324698+MaksiTimur@users.noreply.github.com>
2024-03-15 11:27:06 +0400

    Changed class name for BoardCard title

--- src/components/Boards/BoardCard/BoardCard.jsx
+++ src/components/Boards/BoardCard/BoardCard.jsx
@@ -3,7 +3,7 @@ import './BoardCard.css';
 const Board = ({ data, clickHandler }) => {
     return (
         <div className="board-card" id={data.id} onClick={() => clickHandler(data.id)}>
-            <h2 className="board-title">{data.title}</h2>
+            <h2 className="board-card-title">{data.title}</h2>
         </div>
     )
 }
