ece82dfa21442876173bf3bf219df6175009386b
Timur <144324698+MaksiTimur@users.noreply.github.com>
2024-03-18 21:09:23 +0400

    Removed useDispatch import and declaration from BoardCard component

--- src/components/Boards/BoardCard/BoardCard.jsx
+++ src/components/Boards/BoardCard/BoardCard.jsx
@@ -1,9 +1,6 @@
 import './BoardCard.css';
-import { useDispatch } from 'react-redux';
 
 const BoardCard = ({ data, onClick }) => {
-    const dispatch = useDispatch();
-
     return (
         <div className="board-card" id={data.id} onClick={() => onClick(data.id)}>
             <h2 className="title">{data.title}</h2>
