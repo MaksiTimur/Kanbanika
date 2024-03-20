e3b7bf7c4fca6dbab13a6a6f5fa7ab3b031f2c8a
Timur <144324698+MaksiTimur@users.noreply.github.com>
2024-03-20 15:05:15 +0400

    Deleted currentSlice

--- src/store.js
+++ src/store.js
@@ -3,15 +3,13 @@ import boardsReducer from "./redux/slices/boardsSlice";
 import columnsReducer from "./redux/slices/columnsSlice";
 import tasksReducer from "./redux/slices/tasksSlice";
 import modalReducer from "./redux/slices/modalSlice";
-import currentReducer from "./redux/slices/currentSlice";
 
 const store = configureStore({
         reducer: {
                 boardsReducer,
                 columnsReducer,
                 tasksReducer,
-                modalReducer,
-                currentReducer
+                modalReducer
         }
 });
 
