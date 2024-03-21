51a62e12bd6721eccacff7486cb9b9233126d65b
Timur <144324698+MaksiTimur@users.noreply.github.com>
2024-03-21 14:54:55 +0400

    Added Back button to error page, change some styles

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,16 +1,23 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
+import { FaCircleArrowRight } from "react-icons/fa6";
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+
     const error = useRouteError();
     console.error(error);
 
     return (
         <div id="error-page">
             <h1>Something went wrong!</h1>
-            <p>Sorry, an unexpected error has occurred:</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+            <div className="error-text">
+                <p>Sorry, an unexpected error has occurred:</p>
+                <p>
+                    <i>{error.statusText || error.message}</i>
+                </p>
+            </div>
+            <button onClick={() => navigate('/')}>Go Back <FaCircleArrowRight /></button>
+
         </div>
     );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -22,6 +22,7 @@ const router = createBrowserRouter([
 	{
 		path: "/",
 		loader: () => redirect('/boards'),
+		errorElement: <ErrorPage />,
 	},
 	{
 		path: "/",
