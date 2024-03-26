8f2363f94e9df63b71016df17c4aec151bb79d78
Timur <144324698+MaksiTimur@users.noreply.github.com>
2024-03-26 19:05:51 +0400

    Changing board's background by color/URL in BoardEdit component

--- src/components/Boards/BoardEdit/BoardEdit.jsx
+++ src/components/Boards/BoardEdit/BoardEdit.jsx
@@ -1,9 +1,9 @@
 import { Form } from 'react-router-dom';
 import './BoardEdit.css'
 import { useDispatch, useSelector } from 'react-redux';
-import { setBackground, setTitle } from '../../../redux/slices/boardsSlice';
+import { setActiveBackground, setBackground, setTitle } from '../../../redux/slices/boardsSlice';
 import { setShow } from '../../../redux/slices/modalSlice';
-import { FaRegCircleXmark } from "react-icons/fa6";
+import ChangeBackground from './ChangeBackground/ChangeBackground';
 
 const BoardEdit = () => {
     const boards = useSelector(state => state.boardsReducer).boards;
@@ -20,33 +20,47 @@ const BoardEdit = () => {
 
     const handleSubmit = e => {
         const title = e.target.title.value;
-        const background = e.target.background.value;
-
         if (title.length === 0) return;
 
+        const typesBtns = document.querySelectorAll('.change-bg .bg-types button');
+
+        switch (typesBtns[0].classList[1]) {
+            case 'active':
+                const bgColor = e.target.bgColor.value;
+
+                dispatch(setActiveBackground({ id: board.id, type: 'color' }));
+                dispatch(setBackground({ type: 'color', background: bgColor, id: board.id }));
+
+                break;
+            case undefined:
+                const bgUrl = e.target.bgUrl.value;
+
+                dispatch(setActiveBackground({ id: board.id, type: 'url' }));
+                dispatch(setBackground({ type: 'url', background: bgUrl, id: board.id }));
+
+                break;
+            default:
+                break;
+        }
+
         dispatch(setTitle({ title, id: board.id }));
-        dispatch(setBackground({ background, id: board.id }));
+
         dispatch(setShow({ boardEdit: false }));
-    }
-
-    const resetBgColor = e => {
-        e.preventDefault();
-
-        const inputColor = document.querySelector('.change-bg input');
-
-        inputColor.value = '#1f2229';
     }
 
     return (
         <Form className="board-edit" onSubmit={e => handleSubmit(e)} action=''>
             <label htmlFor="title">Board Title</label>
-            <input type="text" name="title" defaultValue={currentBoard.title} />
+            <input
+                type="text"
+                name="title"
+                onKeyDown={e => {
+                    if (e.key == "Enter") e.preventDefault()
+                }}
+                defaultValue={currentBoard.title}
+            />
 
-            <label htmlFor="background">Column Background</label>
-            <div className="change-bg">
-                <button onClick={e => resetBgColor(e)}><FaRegCircleXmark /></button>
-                <input type="color" name="background" defaultValue={currentBoard.background ?? '#1f2229'} />
-            </div>
+            <ChangeBackground></ChangeBackground>
 
             <button type='submit'>Confirm</button>
         </Form>
